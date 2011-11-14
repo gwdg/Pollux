b895d7e1e2b634b90a597e736773770506da9c62
unknown <MiguelAngel@Lancelot.(none)>
2011-11-14 14:17:18 +0100

    First Example :: how to use 'Pollux' from java stand-alone application

--- cloud.cdmi/src/main/java/cloud/cdmi/internal/extensions/cdmi/restful/XRestfulUtils.java
+++ cloud.cdmi/src/main/java/cloud/cdmi/internal/extensions/cdmi/restful/XRestfulUtils.java
@@ -352,9 +352,12 @@ public class XRestfulUtils
                 put.setRequestHeader( k, headers.get( k ) );
             }
             
+            /* OutofMemory Exception :: HttpClient
             FileInputStream fis = new FileInputStream( body ); 
             InputStreamRequestEntity is = new InputStreamRequestEntity( fis ); 
             put.setRequestEntity( is );
+            */
+            put.setRequestEntity( new ImageRequestEntity( body ) );
             
             HttpClient client = new HttpClient();
             result.code       = client.executeMethod( put );
---
+++ cloud.example/src/main/java/cloud/example/IExample.java
@@ -0,0 +1,83 @@
+package cloud.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cloud.services.one.occi.ICompute;
+import cloud.services.one.occi.IOCCIOneManager;
+
+/**
+ * 
+ * @author Miguel Rojas (email.miguel.rojas@googlemail.com)
+ *
+ */
+public class IExample 
+{
+    public IExample()
+    {
+        init();
+    }
+    
+    protected void init()
+    {
+        try
+        {
+            ApplicationContext ctx = new ClassPathXmlApplicationContext( "META-INF/spring/module-context.xml" );
+            occi = (IOCCIOneManager)ctx.getBean( "occi-one" );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void createVM()
+    {
+        try
+        {
+            ICompute vm = new ICompute();
+            String URI = "http://129.217.211.147:3000";
+            
+            vm.content.put( ICompute.URI         , URI            );
+            vm.content.put( ICompute.TITLE       , "MyVM"         );
+            vm.content.put( ICompute.SUMMARY     , "MyVM-summary" );
+            vm.content.put( ICompute.ARCHITECTURE, "x64"          );
+            vm.content.put( ICompute.CORES       , "2"            );
+            vm.content.put( ICompute.MEMORY      , "32"           );
+            vm.content.put( ICompute.NETWORK     , "networkID"    );
+            vm.content.put( ICompute.STORAGE     , "storageID"    );
+                
+            occi.createCompute( vm );
+            occi.startCompute ( vm );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void dumpVMs()
+    {
+        try
+        {
+            ICompute[] computes = occi.getComputes( "http://129.217.211.147:3000" );
+            if ( computes != null )
+                for ( ICompute s : computes )
+                {
+                    System.out.println( s );
+                }
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main( String[] args )
+    {
+        IExample ex = new IExample();
+        ex.createVM();
+    }
+    
+    protected IOCCIOneManager occi;
+}
