0a9d6c5c2b3342ca614eb0ba38400d95968b72c7
Miguel Angel Rojas Gonzalez <email.miguel.rojas@googlemail.com>
2011-11-16 15:53:48 +0100

    Multithreading added for getting information about Storage and Network

--- cloud.one.occi/src/main/java/cloud/one/occi/internal/OcciExecutorCompletionService.java
+++ cloud.one.occi/src/main/java/cloud/one/occi/internal/OcciExecutorCompletionService.java
@@ -12,9 +12,89 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import cloud.services.one.occi.ICompute;
+import cloud.services.one.occi.INetwork;
+import cloud.services.one.occi.IStorage;
 
 public class OcciExecutorCompletionService
 {
+    public void fetchNetworkInformation( Hashtable<String, String> headers, INetwork[] networks )
+    {
+        try
+        {
+            int poolSize = networks.length*40/100;
+            if ( poolSize < 5 ) poolSize = 5;
+            
+            System.out.println( "Using OcciExecutorCompletionService:poolSize="+poolSize );
+            ExecutorService pool = Executors.newFixedThreadPool( poolSize );
+            ArrayList<OCCiNetworkInfoFetcher> al = new ArrayList<OCCiNetworkInfoFetcher>();
+            
+            for ( int i = 0; i < networks.length; i++ )
+                al.add( new OCCiNetworkInfoFetcher( headers, networks[ i ] ) );
+            
+            runNetworksInParallel( pool, al );
+            pool.shutdown();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void runNetworksInParallel( Executor e, Collection<? extends Callable<INetwork>> nets ) throws InterruptedException, ExecutionException
+    {
+        CompletionService<INetwork> ecs = new ExecutorCompletionService<INetwork>( e );
+        for ( Callable<INetwork> s : nets )
+            ecs.submit( s );
+        int n = nets.size();
+        for ( int i = 0; i < n; ++i )
+        {
+            INetwork r = ecs.take().get();
+            if ( r != null )
+            { 
+                // consume Net ( r )
+            }
+        }
+    }
+    
+    public void fetchStorageInformation( Hashtable<String, String> headers, IStorage[] storages )
+    {
+        try
+        {
+            int poolSize = storages.length*40/100;
+            if ( poolSize < 5 ) poolSize = 5;
+            
+            System.out.println( "Using OcciExecutorCompletionService:poolSize="+poolSize );
+            ExecutorService pool = Executors.newFixedThreadPool( poolSize );
+            ArrayList<OCCiStorageInfoFetcher> al = new ArrayList<OCCiStorageInfoFetcher>();
+            
+            for ( int i = 0; i < storages.length; i++ )
+                al.add( new OCCiStorageInfoFetcher( headers, storages[ i ] ) );
+            
+            runStoragesInParallel( pool, al );
+            pool.shutdown();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void runStoragesInParallel( Executor e, Collection<? extends Callable<IStorage>> storages ) throws InterruptedException, ExecutionException
+    {
+        CompletionService<IStorage> ecs = new ExecutorCompletionService<IStorage>( e );
+        for ( Callable<IStorage> s : storages )
+            ecs.submit( s );
+        int n = storages.size();
+        for ( int i = 0; i < n; ++i )
+        {
+            IStorage r = ecs.take().get();
+            if ( r != null )
+            { 
+                // consume Storage ( r )
+            }
+        }
+    }
+    
     public void fetchVmInformation( Hashtable<String, String> headers, ICompute[] vms )
     {
         try
@@ -53,6 +133,8 @@ public class OcciExecutorCompletionService
             }
         }
     }
+    
+    // -- HELPER CLASSES --------------------------------------------------------------
     
     public class OCCiVmInfoFetcher implements Callable<ICompute>
     {
@@ -103,6 +185,110 @@ public class OcciExecutorCompletionService
             }
             
             return this.vm;
+        }
+    }
+    
+    public class OCCiStorageInfoFetcher implements Callable<IStorage>
+    {
+        Hashtable<String, String> headers;
+        IStorage storage;
+        
+        public OCCiStorageInfoFetcher( Hashtable<String, String> headers, IStorage vm )
+        {
+            this.headers = headers;
+            this.storage = vm;
+        }
+        
+        public IStorage call() throws Exception
+        {
+            // Details about this Compute
+            String URI = this.storage.getContent().get( IStorage.URI );
+            
+            OcciResponse inner = HttpUtils.get( URI, headers );
+            if ( inner != null && inner.content != null )
+            {
+                for ( String attr : inner.content )
+                {
+                    if ( attr.contains( "-Attribute" ) )
+                    {
+                        String ATT = attr.substring( attr.indexOf( "Attribute" ) + 11 );
+                        String[] tokens = ATT.split( "=" );
+                        
+                        if ( tokens.length >= 2 )
+                        {
+                            //remove extra """
+                            tokens[ 0 ] = ATT.substring( 0, ATT.indexOf( "=" ) ).replace( "\"", "" );
+                            tokens[ 1 ] = ATT.substring( ATT.indexOf( "=" )+1 ).replace( "\"", "" );
+                        }
+                        else if ( tokens.length == 1 )
+                        {
+                            tokens = new String[]{ tokens[ 0 ].replace( "\"", "" ), "" };
+                        }
+                        
+                        this.storage.getAttributes().put( tokens[ 0 ], tokens[ 1 ] );
+                    }
+                    else if ( attr.startsWith( "Link" ) ) // Format[  Link: </api/compute/ea97ad10-8272-4df3-a9dc-484b0aa75902?action=start>; ... ]
+                    {
+                        String linkCnt = attr.substring( attr.indexOf( "<" )+1, attr.indexOf( ">" ) );
+                        
+                        this.storage.getLinks().add( linkCnt );
+                    }
+                }
+            }
+            
+            return this.storage;
+        }
+    }
+    
+    public class OCCiNetworkInfoFetcher implements Callable<INetwork>
+    {
+        Hashtable<String, String> headers;
+        INetwork net;
+        
+        public OCCiNetworkInfoFetcher( Hashtable<String, String> headers, INetwork net )
+        {
+            this.headers = headers;
+            this.net     = net;
+        }
+        
+        public INetwork call() throws Exception
+        {
+            // Details about this Compute
+            String URI = this.net.getContent().get( ICompute.URI );
+            
+            OcciResponse inner = HttpUtils.get( URI, headers );
+            if ( inner != null && inner.content != null )
+            {
+                for ( String attr : inner.content )
+                {
+                    if ( attr.contains( "-Attribute" ) )
+                    {
+                        String ATT = attr.substring( attr.indexOf( "Attribute" ) + 11 );
+                        String[] tokens = ATT.split( "=" );
+                        
+                        if ( tokens.length >= 2 )
+                        {
+                            //remove extra """
+                            tokens[ 0 ] = ATT.substring( 0, ATT.indexOf( "=" ) ).replace( "\"", "" );
+                            tokens[ 1 ] = ATT.substring( ATT.indexOf( "=" )+1 ).replace( "\"", "" );
+                        }
+                        else if ( tokens.length == 1 )
+                        {
+                            tokens = new String[]{ tokens[ 0 ].replace( "\"", "" ), "" };
+                        }
+                        
+                        this.net.getAttributes().put( tokens[ 0 ], tokens[ 1 ] );
+                    }
+                    else if ( attr.startsWith( "Link" ) ) // Format[  Link: </api/compute/ea97ad10-8272-4df3-a9dc-484b0aa75902?action=start>; ... ]
+                    {
+                        String linkCnt = attr.substring( attr.indexOf( "<" )+1, attr.indexOf( ">" ) );
+                        
+                        this.net.getLinks().add( linkCnt );
+                    }
+                }
+            }
+            
+            return this.net;
         }
     }
 }
