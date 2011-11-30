b1251f088f354390b24068110c8a6335f34bb968
Miguel Angel Rojas Gonzalez <email.miguel.rojas@googlemail.com>
2011-11-30 14:54:51 +0100

    deploy.bat updated.

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
-set CLOUD_BIN=%DROPBOX%\Cloud\virgo\cloud\cloud\target
+set CLOUD_BIN=.\cloud\target
 set CLOUD_LIBS=%CLOUD_BIN%\par-provided
-set VIRGO=D:\mr\environment\virgo-tomcat-server-3.0
+set VIRGO=%VIRGO_WEB_SERVER%
 set VIRGO_REPO=%VIRGO%\repository\usr
 copy %CLOUD_LIBS%\*.* %VIRGO_REPO%
 copy %CLOUD_BIN%\cloud.par-1.0.par %VIRGO%\pickup
